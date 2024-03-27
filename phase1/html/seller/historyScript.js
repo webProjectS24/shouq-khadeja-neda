b323d91476b0178c4a354ae860f587816062f500
shouq-sa2001784 <sa2001784@qu.edu.qa>
2024-03-27 14:56:16 +0300

    itemhistory js fininshed

--- phase1/html/seller/historyScript.js
+++ phase1/html/seller/historyScript.js
@@ -1,6 +1,8 @@
 const itemsContainer = document.querySelector("#items-container")
+const itemCard = document.querySelector("#item-card")
 const usersJson = '../data/user.json'
 let accounts = []
+let items =[]
 document.addEventListener('DOMContentLoaded', function () {
     loadItems()
 })
@@ -13,7 +15,7 @@ async function loadItems(){
         else{
           accounts = JSON.parse(localStorage.accounts)
         }
-    let items = findAccountItems(accounts)
+    items = findAccountItems(accounts)
     displayItems(items)
 }
 function findAccountItems(accounts){
@@ -30,7 +32,7 @@ function displayItems(items){
 function itemToHTML(item){
     return `
     <div class="items-container" id="items-container">
-    <div class="item-card">
+    <div class="item-card" id="item-card">
         <img class="item-image" src="${item.imageUrl}">
         <div class="item-details">
             <h2 class="item-title">${item.name}</h2>
@@ -42,3 +44,26 @@ function itemToHTML(item){
     </div>
     `
 }
+// function showDetails(itemNo){
+//     let item = items.find(item => itemNo == item.itemNo)
+//     itemCard.innerHTML = `
+//     <div class="itemDetails">
+//     <div class="details">
+//         <img src="/images/image copy.png" alt="Item 1">
+//         <div class="content">
+//         <h2 class="title">Item 1</h2>
+//         <p class="description">Description of Item 1</p>
+//         <p class="price">$10.99</p>
+//         <p class="quantity">Available: 5</p>
+//         <h3>Sale History</h3>
+//         <h4>Total number of items sold: 3</h4>
+//         <ul class="saleHistory">
+//             <li>Buyer: user1, Price: 18.99 QR</li>
+//             <li>Buyer: user2, Price: 19.99 QR</li>
+//             <li>Buyer: user3, Price: 17.99 QR</li>
+//         </ul>
+//     </div>
+//     </div>
+// </div>
+//     `
+// }
