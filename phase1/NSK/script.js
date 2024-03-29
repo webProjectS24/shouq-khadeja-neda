886235cd076352a9717d1ef1cf8b7e734bf5242c
shouq-sa2001784 <sa2001784@qu.edu.qa>
2024-03-30 02:26:45 +0300

    fixing css and seller products

--- phase1/NSK/script.js
+++ phase1/NSK/script.js
@@ -44,7 +44,8 @@ async function loadProducts() {
     products = JSON.parse(localStorage.items);
   }
   let filteredProducts = products.filter((product) => product.quantity != 0);
-  displayProducts(filteredProducts);
+  let filtersellerPorducts = filteredProducts.filter(product => product.sellerId != accountNo)
+  displayProducts(filtersellerPorducts);
 }
 function displayProducts(products) {
   let htmlForProducts = products
@@ -102,114 +103,6 @@ function buyProduct(itemNo, sellerId) {
     window.location.href = `./buyProduct.html?accountNo=${accounts[index].accountNo}&itemNo=${itemNo}&sellerId=${sellerId}`;
   }
 }
-
-//  HEAD
-// items.forEach((item) => {
-//   const itemElement = document.createElement("div");
-//   itemElement.classList.add("item");
-// });
-
-//reading from flie
-
-// const fs = require('fs');
-// async function readItemsFromFile(){
-//     try{
-//         const data = await fs.promises.readFole('./repo/itemRepo.js')
-//     }
-// }
-
-//search in json file
-// const searchInput = document.getElementById('#search-input');
-// // const searchButton = document.getElementById();
-// const searchResultList = document.getElementById('');
-// function searchItems(){
-//     let searchInput = document.getElementById('search-input').ariaValueMax.toLowerCase();
-//     fetch('item.json')
-//     .then(response=> response.json())
-//     .then(data=>{
-//     })
-
-// }
-
-//test code
-// let http = new XMLHttpRequest();
-// http.open('get','./repo/itemRepo.js',true);
-// http.send();
-// http.onload = function(){
-//     if(this.readyState == 4 && this.status == 200){
-//         let products = JSON.parse(this.responseText);
-//         let output ="";
-
-//         for(let item of products){
-//             output += ' <div class="products"> <img src="" alt=""> <p class="title"></p> <p class=""></p> </div>';
-//         }
-
-//     }
-
-// item
-// let accounts=[];
-// try{
-//     const storedAcc = localStorage.getItem('accounts');
-//     if(storedAcc){
-//         accounts=JSON.parse(storedAcc);
-//     }
-// } catch(error){
-//     console.error('error from localStorage',error);
-// }
-// const buyButton = document.getElementById('');
-
-// cart
-// document.addEventListener('DOMContentLoaded', ()=>{
-//     const cartItemList = document.querySelector('.cartItems');
-//     const cartTotal = document.querySelector('.cartTotal');
-//     const cartIcon = document.querySelector('#cart-icon');
-//     const cartSide = document.getElementById('.cartSide');
-
-//     let cartItems =[];
-//     let totalAmount = 0;
-
-// })
-
-// chechout
-
-// const itemIndex = items.findIndex((item) => item.itemNo === itemNo);
-// const customer = accounts.find((account) => account.accountNo === accountNo);
-// const sellerIndex = accounts.findIndex(
-//   (account) => account.accountNo === sellerId
-// );
-
-// //chech is found
-// if (itemIndex !== -1 && customer && sellerIndex !== -1) {
-//   if (items[itemIndex].price > customer.balance) {
-//     alert("Your balance is not enough!");
-//     return;
-//   }
-// } else if (quantity > items[itemIndex].quantity) {
-//   alert("Quantity is greater than the number of available items ");
-//   return;
-// }
-
-// try {
-//   items[itemIndex].sold += quantity;
-//   items[itemIndex].quantity -= quantity;
-
-//   if (items[itemIndex].quantity === 0) {
-//     item[itemIndex].status = "sold";
-//   }
-//   customer.items.push(items[index]);
-
-//   if (accounts[sellerIndex].hasOwnProperty("soldItem")) {
-//     accounts[sellerIndex].soldItem.push(items[itemIndex]);
-//   } else {
-//     accounts[sellerIndex].soldItem = [items[itemIndex]];
-//   }
-
-//   console.log("Successful puurchase");
-// } catch (error) {
-//   console.error("Error", error);
-//   alert("Please try again later");
-// }
-
 // =======
 //logout button
 logoutbtn.addEventListener("click", logOut);
