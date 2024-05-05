a52ad0edc0898c2a16ce19cbe4406f308d32f519
shouq-sa2001784 <sa2001784@qu.edu.qa>
2024-05-06 00:18:20 +0300

    remove unnecessary files

--- Phase2/app/api/books/summary/route.js
+++
@@ -1,7 +0,0 @@
-import booksRepo from "@/app/repo/BooksRepo.js";
-
-// Returns a map of author names with the count of books they have authored
-export async function GET(request) {
-    const books = await booksRepo.getBooksSummary()
-    return Response.json(books, { status: 200 })
-}
