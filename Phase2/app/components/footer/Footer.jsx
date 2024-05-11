8bc06b060cb1e8aca8b45c0d48aa49f01a17d714
NadaMohamm <na1802920@qu.edu.qa>
2024-05-11 12:11:46 +0300

    update footer.jsx

--- Phase2/app/components/footer/Footer.jsx
+++ Phase2/app/components/footer/Footer.jsx
@@ -1,56 +1,30 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer
-      style={{
-        backgroundColor: "bleck",
-        color: "white",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        className="footer-container"
-        style={{ display: "flex", gap: "15px", flexGrow: 1 }}
-      >
-        <div className="column" style={{ flex: 1 }}>
+    <footer className="bg-black text-white flex items-center h-160 px-4">
+     <div className="footer-container flex gap-20 flex-grow">
+     <div className="column flex-1">
           <div className="row">
-            <h3 style={{ textDecoration: "underline", color: "white" }}>
-              About Us
-            </h3>
-            <p>
-              Welcome to NSK We are a passionate team dedicated to providing
-              unique experience.
-            </p>
+            <h3 className="underline">About Us</h3>
+            <p>Welcome to NSK We are a passionate team dedicated to providing unique experience.</p>
           </div>
-        </div>
-        <div className="column" style={{ flex: 1 }}>
+          </div>
+          <div className="column" style={{ flex: 1 }}>
           <div className="row ">
-            <h3 style={{ textDecoration: "underline", color: "white" }}>
-              {" "}
-              Contact Us
-            </h3>
+            <h3 className="underline"> Contact Us</h3>
             <p>Email: info@example.com</p>
             <p>Phone: +1234567890</p>
           </div>
-        </div>
-
+          </div>
+        
         <div className="column" style={{ flex: 2 }}>
           <div className="row">
-            <ul style={{ listStyleType: "none", padding: 0 }}>
-              <li>
-                <a href="/">Home</a>
-              </li>
-              <li>
-                <a href="/products">Products</a>
-              </li>
-              <li>
-                <a href="/about">About Us</a>
-              </li>
-              <li>
-                <a href="/contact">Contact Us</a>
-              </li>
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              <li><a href="/">Home</a></li>
+              <li><a href="/products">Products</a></li>
+              <li><a href="/about">About Us</a></li>
+              <li><a href="/contact">Contact Us</a></li>
             </ul>
           </div>
           {/* <div className="row">
@@ -63,11 +37,9 @@ const Footer = () => {
             </div>
           </div> */}
         </div>
-        <div className="column" style={{ flex: 1 }}>
+        <div className="column flex-1">
           <div className="row">
-            <h3 style={{ textDecoration: "underline", color: "white" }}>
-              Newsletter
-            </h3>
+            <h3 className="underline">News letter</h3>
             <form>
               <input type="email" placeholder="Enter your email" />
               <button type="submit">Subscribe</button>
@@ -75,10 +47,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div
-        className="copy-right"
-        style={{ textAlign: "center", marginTop: "90px" }}
-      >
+      <div className="copy-right text-center mt-9">
         &copy; 2024 NSK. All rights reserved.
       </div>
     </footer>
